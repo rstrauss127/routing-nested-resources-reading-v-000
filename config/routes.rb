2ec1ffa537f5d7734a768c9424cef85e7760ea67
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-01 18:41:43 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'authors/:id/posts', to: 'authors#posts_index'
-  get 'authors/:id/posts/:post_id', to: 'authors#post'
-  resources :authors, only: [:show]
+
+  resources :authors, only: [:show] do 
+    resources :posts, only: [:show, :index]
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
