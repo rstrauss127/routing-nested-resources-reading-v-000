9f659bdd8228ed14085a2047514244fc2bf7bf06
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-01 18:29:53 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,10 @@
 class AuthorsController < ApplicationController
 
+  def posts_index
+    @author = Author.find(params[:id])
+    @posts = @author.posts
+    render template: 'posts/index'
+  end
   def show
     @author = Author.find(params[:id])
   end
